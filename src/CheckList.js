3845554e215a5079ed35e92d6130cface31a0079
Arnaud J <arnaudj@users.noreply.github.com>
2017-01-18 22:11:31 +0000

    Add auto input focus for new task input

--- src/CheckList.js
+++ src/CheckList.js
@@ -14,6 +14,12 @@ class CheckList extends Component {
         return (<div className="checklist">
             {/* Expand tasks: */}
             <ul>{tasks}</ul>
+            <input type="text"
+                className="checklist--add-task"
+                placeholder="Type then hit Enter to add a task"
+                ref={ // called upon component mount (reference) or after dismount (null, for GC) - http://reactjs.cn/react/docs/more-about-refs.html
+                    (input) => input != null ? input.focus() : null
+                } />
         </div>
         );
     }
